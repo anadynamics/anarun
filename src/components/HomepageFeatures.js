87c0d33d50f839714520c48780b0570e4aa591a7
pgbarletta <pbarletta@gmail.com>
2021-12-21 17:36:35 +0100

    switching from png images to svg

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Fast',
-    Svg: require('../../static/img/cpp_logo.svg').default,
+    Png: require('../../static/img/cpp_logo.png').default,
     description: (
       <>
        Made for long molecular dynamics trajectories. 
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Maximum control',
-    Svg: require('../../static/img/1hvr_apo_ch.svg').default,
+    Png: require('../../static/img/1hvr_apo_ch.png').default,
     description: (
       <>
         Define your cavity both manually and easily by listing the lining residues.
@@ -24,7 +24,7 @@ const FeatureList = [
   },
   {
     title: 'Adaptive cavity definition',
-    Svg: require('../../static/img/adaptive.svg').default,
+    Png: require('../../static/img/adaptive.png').default,
     description: (
       <>
         Follow cavity changes along large conformational changes.
@@ -34,7 +34,7 @@ const FeatureList = [
   },
   {
     title: 'Cavity flexibility',
-    Svg: require('../../static/img/abstract_fig_cut.svg').default,
+    Png: require('../../static/img/abstract_fig_cut.png').default,
     description: (
       <>
         Predict flexibility using a set of collective coordinates as Normal Modes or Principal Components.
@@ -43,6 +43,19 @@ const FeatureList = [
   },
 ];
 
+function FeaturePNG({Png, title, description}) {
+  return (
+    <div className={clsx('col col--3')}>
+      <div className="text--center">
+        <img src={Png} className={styles.featurePng} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--3')}>
@@ -63,7 +76,7 @@ export default function HomepageFeatures() {
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <FeaturePNG key={idx} {...props} />
           ))}
         </div>
       </div>
